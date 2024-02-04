15cd3c4961e5746725e0372fd9abdf2a7cf8e19d
Svyatoslav Petrov <sp.gro.33@gmail.com>
2024-02-04 19:11:36 +0300

    Add tasks

---
+++ Mod10.5.1/Tasks/Program.cs
@@ -0,0 +1,91 @@
+﻿using Task1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Event(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void Error(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    public interface ISum
+    {
+        void Sum(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine(result);
+        }
+    }
+    public class Summation : ISum
+    {
+        ILogger logger { get; }
+        public Summation()
+        {}
+        public Summation (ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void Sum(int num1,int num2)
+        {
+            int result=num1+num2;
+            Console.BackgroundColor = ConsoleColor.Blue;   
+            logger.Event("Расчет произведен, сумма чисел равна:");
+            Console.WriteLine(result);
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                ILogger Logger;
+                Logger=new Logger();
+                Console.WriteLine("Please enter two numbers");
+
+                int num1;
+                bool res= int.TryParse(Console.ReadLine(), out num1);
+
+                int num2;
+                bool res1 = int.TryParse(Console.ReadLine(), out num2);
+
+                if (res == false|| res1==false)
+                {
+                    throw new UserException("The number input invalid format");
+                }
+                ISum sum = new Summation(Logger);
+
+                sum.Sum(num1,num2);
+
+                Console.ReadKey();
+            }
+            catch (ArgumentException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+    }
+}
---
+++ Mod10.5.1/Tasks/UserException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Task1;
+
+public class UserException : Exception
+{
+    ILogger logger;
+    public UserException(string message)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        logger.Error(message);
+    }
+    public UserException(ILogger logger)
+    {
+        this.logger = logger;
+    }
+    
+}
